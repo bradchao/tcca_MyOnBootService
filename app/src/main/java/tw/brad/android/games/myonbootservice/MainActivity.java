8b69e6511363e6c0c3b2d8c866538b5590ae759f
Brad <brad@brad.tw>
2017-08-18 19:19:21 +0800

    v2

--- app/src/main/java/tw/brad/android/games/myonbootservice/MainActivity.java
+++ app/src/main/java/tw/brad/android/games/myonbootservice/MainActivity.java
@@ -1,13 +1,41 @@
 package tw.brad.android.games.myonbootservice;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView img;
+    private View stop;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        img = (ImageView)findViewById(R.id.start);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this,
+                        MyService.class);
+                startService(it);
+            }
+        });
+
+        stop = findViewById(R.id.stop);
+        stop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent it = new Intent(MainActivity.this,
+                        MyService.class);
+                stopService(it);
+            }
+        });
+
     }
+
+
 }
